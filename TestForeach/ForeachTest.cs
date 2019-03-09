41664ab3efeae15818ec1a9b0af0a92ebae65fa0
destiny130 <destiny1307654641@gamil.com>
2019-03-09 18:35:31 +0800

    Initial commit

---
+++ TestForeach/ForeachTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForeach
+{
+    class ForeachTest
+    {
+        static void Main(string[] args)
+        {
+            string str = "1,2,3,4,5,6";
+            foreach (int i in Read(str))
+                Console.Write($"{i}\t");
+            Console.ReadKey();
+        }
+
+        static List<int> Read(string str)
+        {
+            Console.Write("Invoke me\n");
+            return new List<int>() { 1, 2, 3, 4, 5 };
+        }
+    }
+}
---
+++ TestTuple/TupleTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTuple
+{
+    class TupleTest
+    {
+        static void Main(string[] args)
+        {
+            Tuple<Test, Test> tuple = new Tuple<Test, Test>(new Test(1, "1"), new Test(2, "2"));
+            //tuple.Item1 = new Test();  //Compile error
+            Test t1 = tuple.Item1;
+            t1.ID = 3;
+            t1.Name = "3";
+            Test t2 = tuple.Item2;
+            t2 = new Test(4, "4");
+            Console.WriteLine($"Item1: {tuple.Item1.ID}, {tuple.Item1.Name}");
+            Console.WriteLine($"Item2: {tuple.Item2.ID}, {tuple.Item2.Name}");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Test
+    {
+        public int ID;
+        public string Name;
+
+        public Test()
+        {
+
+        }
+
+        public Test(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+    }
+}
