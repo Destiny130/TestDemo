27aeabf30f5d7e37683263bda4df348dab91b2df
Destiny130 <destiny1307654641@hotmail.com>
2019-04-12 16:50:31 +0800

    Update AsyncExecute.cs

--- TestDemo/FunctionTest/AsyncExecute.cs
+++ TestDemo/FunctionTest/AsyncExecute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,16 +9,18 @@ namespace TestDemo.FunctionTest
         public async void Execute()
         {
             Task task = null;
+            Task task2 = null;
             for (int i = 0; i < 5; ++i)
             {
                 if (i == 0)
                 {
-                    task = Test();
+                    task = Task.Run(() => Test());
+                    task2 = Task.Run(() => Test2());
                 }
                 Console.WriteLine($"{i.ToString()} in for loop. \t\t{DateTime.Now:HH:mm:ss}");
                 Thread.Sleep(2000);
             }
-            await task;
+            await Task.WhenAll(task, task2);
             Console.WriteLine($"Execute end. \t\t{DateTime.Now:HH:mm:ss}");
         }
 
@@ -31,8 +33,21 @@ namespace TestDemo.FunctionTest
                 Thread.Sleep(2000);
             }
 
-            await Task.Delay(17000);
+            await Task.Delay(10);
             Console.WriteLine($"Async end. \t\t{DateTime.Now:HH:mm:ss}");
+        }
+
+        async Task Test2()
+        {
+            Console.WriteLine($"Async2 start. \t\t{DateTime.Now:HH:mm:ss}");
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine($"{i.ToString()} in async2 for loop.\t{DateTime.Now:HH:mm:ss}");
+                Thread.Sleep(2000);
+            }
+
+            await Task.Delay(10);
+            Console.WriteLine($"Async2 end. \t\t{DateTime.Now:HH:mm:ss}");
         }
     }
 }
