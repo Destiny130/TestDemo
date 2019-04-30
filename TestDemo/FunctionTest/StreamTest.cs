c13678f680e03f65c4069bff872923cb39f7e255
Destiny130 <destiny1307654641@hotmail.com>
2019-04-30 17:00:42 +0800

    Create StreamTest.cs

---
+++ TestDemo/FunctionTest/StreamTest.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestDemo.FunctionTest
+{
+    public class StreamTest
+    {
+        public void Start()
+        {
+            SimpleExample();
+
+
+
+        }
+
+        #region Stream simple example
+
+        void SimpleExample()
+        {
+            string srcStr = "Stream. Hello world!";
+            string dstStr = String.Empty;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                print($"Original string is: {srcStr}");
+                if (ms.CanWrite)
+                {
+                    //Write source string into memory stream
+                    byte[] buffer = Encoding.Default.GetBytes(srcStr);
+                    ms.Write(buffer, 0, 3);
+                    print($"Stream.Position: {ms.Position}");
+                    long newPosition = ms.CanSeek ? ms.Seek(3, SeekOrigin.Current) : 0;
+                    print($"After seek, stream.Position: {ms.Position}");
+                    if (newPosition < buffer.Length)
+                    {
+                        ms.Write(buffer, (int)newPosition, buffer.Length - (int)newPosition);
+                    }
+
+                    //Write ms into destination string
+                    ms.Position = 0;
+                    byte[] readBuffer = new byte[ms.Length];
+                    int count = ms.CanRead ? ms.Read(readBuffer, 0, (int)ms.Length) : 0;
+                    int charCount = Encoding.Default.GetCharCount(readBuffer, 0, count);
+                    char[] readCharArr = new char[charCount];
+                    Encoding.Default.GetDecoder().GetChars(readBuffer, 0, count, readCharArr, 0);
+                    for (int i = 0; i < readCharArr.Length; ++i)
+                    {
+                        dstStr += readCharArr[i];
+                    }
+                    print($"Read string is: {dstStr}");
+                }
+                ms.Close();
+            }
+        }
+
+        #endregion Stream simple example
+
+        #region Delegates
+
+        Action<string> print = str => Console.WriteLine(str);
+
+        #endregion Delegates
+    }
+}
