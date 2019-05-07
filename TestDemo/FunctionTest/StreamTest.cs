c8a6c54bab926fe00316ee2a6d85a327f823debb
Destiny130 <destiny1307654641@hotmail.com>
2019-05-07 17:10:23 +0800

    Update StreamTest.cs

--- TestDemo/FunctionTest/StreamTest.cs
+++ TestDemo/FunctionTest/StreamTest.cs
@@ -10,7 +10,9 @@ namespace TestDemo.FunctionTest
         {
             //SimpleExample();
 
-            TextReaderTest();
+            //TextReaderTest();
+
+            FileStreamTest();
         }
 
         #region Stream simple example
@@ -84,9 +86,99 @@ namespace TestDemo.FunctionTest
 
         #endregion TextReader test
 
+        #region FileStream test
+
+        void FileStreamTest()
+        {
+            string filePath = "../../../Input/Stream Source/TextReader.txt";
+            char[] buffer = new char[3];
+
+            using (FileStream fs = File.OpenRead(filePath))
+            using (StreamReader reader = new StreamReader(fs))
+            {
+                DisplayByUsingRead(reader);
+            }
+
+            using (FileStream fs = File.OpenRead(filePath))
+            using (StreamReader reader = new StreamReader(fs, Encoding.ASCII, false))
+            {
+                DisplayByUsingReadBlock(reader);
+            }
+
+            using (StreamReader reader = new StreamReader(filePath, Encoding.Default, false, 123))
+            {
+                DisplayByUsingReadLine(reader);
+            }
+
+            using (StreamReader reader = File.OpenText(filePath))
+            {
+                DisplayByUsingReadLine(reader);
+            }
+        }
+
+        void DisplayByUsingRead(StreamReader reader)
+        {
+            int readChar = 0;
+            StringBuilder sb = new StringBuilder();
+            while ((readChar = reader.Read()) != -1)
+            {
+                sb.Append((char)readChar);
+            }
+            print($"Using StreamReader.Read() get text content: {sb.ToString()}");
+        }
+
+        void DisplayByUsingReadBlock(StreamReader reader)
+        {
+            char[] buffer = new char[10];
+            StringBuilder sb = new StringBuilder();
+            reader.ReadBlock(buffer, 0, 10);
+            for (int i = 0; i < buffer.Length; ++i)
+            {
+                sb.Append(buffer[i]);
+            }
+            print($"Using StreamReader.ReadBlock() get text first 10 content: {sb.ToString()}");
+        }
+
+        void DisplayByUsingReadLine(StreamReader reader)
+        {
+            int i = 1;
+            string result = String.Empty;
+            while ((result = reader.ReadLine()) != null)
+            {
+                print($"Using StreamReader.ReadLine() get text {ordinalFunc(i)} line content: {result}");
+                ++i;
+            }
+        }
+
+        #endregion FileStream test
+
         #region Delegates
 
         Action<string> print = str => Console.WriteLine(str);
+
+        Func<int, string> ordinalFunc = i => i % 10 == 1 ? $"{i}st" : (i % 10 == 2 ? $"{i}nd" : (i % 10 == 3 ? $"{i}rd" : $"{i}th"));
+
+        Func<int, string> ordinalFunc2 = i =>
+        {
+            int mod = i % 10;
+            string result = i.ToString();
+            switch (mod)
+            {
+                case 1:
+                    result += "st";
+                    break;
+                case 2:
+                    result += "nd";
+                    break;
+                case 3:
+                    result += "rd";
+                    break;
+                default:
+                    result += "th";
+                    break;
+            }
+            return result;
+        };
 
         #endregion Delegates
     }
