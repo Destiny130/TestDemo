061de562c1cd4a2573bb54d6d149d89500083403
Destiny130 <destiny1307654641@hotmail.com>
2019-06-28 14:50:45 +0800

    Update AsyncForm.cs

--- TestDemo/FunctionTest/AsyncForm.cs
+++ TestDemo/FunctionTest/AsyncForm.cs
@@ -1,41 +1,64 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace TestDemo.FunctionTest
 {
     public class AsyncForm : Form
     {
-        Label label;
+        const string URL = "https://auth.alipay.com/login/index.htm";
         Button button;
+        Label label;
 
         public AsyncForm()
         {
-            label = new Label() { Location = new Point(10, 20), Height = 40, Text = "Length" };
-            button = new Button() { Location = new Point(10, 50), Text = "Click" };
+            button = new Button() { Location = new Point(10, 15), Text = "Click" };
             button.Click += DisplayWebSiteLength;
-            AutoSize = true;
-            Controls.Add(label);
+            label = new Label() { Location = new Point(10, 40), Height = 300, Width = 200, Text = "Length" };
             Controls.Add(button);
+            Controls.Add(label);
+            AutoSize = true;
+            CenterToScreen();
         }
 
         async void DisplayWebSiteLength(object sender, EventArgs e)
         {
             label.Text = "Fetching...";
-            using (HttpClient client = new HttpClient())
+            HttpClient client = new HttpClient();
+            try
             {
-                //string text = await client.GetStringAsync("https://auth.alipay.com/login/index.htm");
+                //string text = await client.GetStringAsync(URL);
 
-                //Task<string> task = client.GetStringAsync("https://auth.alipay.com/login/index.htm");  //Start wait
+                //Task<string> task = client.GetStringAsync(URL);  //Start wait
                 //string text = await task;
 
-                Task<string> task = Task.Run(() => client.GetStringAsync("https://auth.alipay.com/login/index.htm"));
-                string text = await task;  //Start wait
+                //Task<string> task = Task.Run(() => client.GetStringAsync(URL));
+                //string text = await task;  //Start wait
 
-                label.Text = text.Length.ToString() + " " + text;
+                Task<string> task = Task.Factory.StartNew<string>(() => DelayReturn(client, URL).Result);
+                string text = await task;  //Start wait
+                label.Text = text.Length.ToString() + "\n " + text;
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}\n", ex);
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Dispose();
+                }
+            }
+        }
+
+        Task<string> DelayReturn(HttpClient client, string url)
+        {
+            Thread.Sleep(2 * 1000);
+            return client.GetStringAsync(url);
         }
     }
 }
